522990f99bbe4a01f148da503f208a7506f3805f
GDRiveros <gonzalodaniel.r17@gmail.com>
2022-10-02 23:12:23 -0300

    Intento fallido de uso de datetime (comentado)

--- PrimerMVT/AppMVT/views.py
+++ PrimerMVT/AppMVT/views.py
@@ -5,30 +5,36 @@ from django.shortcuts import render
 from django.template import loader, Context
 from django.http import HttpResponse
 from AppMVT.models import Familiares
+#from datetime import datetime
 
 
 def ver_familiares(request):
     template = loader.get_template("template.html")
 
+    #nacimiento_familiar1 = datetime(1979, 12, 5)
+    #nacimiento_familiar2 = datetime(1978, 5, 8)
+    #nacimiento_familiar3 = datetime(1945, 8, 17)
+
+
     familiar1 = Familiares(
         nombre="Luciana",
         apellido="Slutzker",
         numero_de_telefono= 1182967848,
-        fecha_de_nacimiento="1979-12-05"
+        fecha_de_nacimiento= "1979-12-05"               #nacimiento_familiar1.strftime("%Y - %m - %d")
     )
 
     familiar2 = Familiares(
         nombre="Leonardo",
         apellido="Riveros",
         numero_de_telefono= 1155356478,
-        fecha_de_nacimiento="1978-05-08"
+        fecha_de_nacimiento= "1978-05-08"                #nacimiento_familiar2.strftime("%Y - %m - %d")
     )
 
     familiar3 = Familiares(
         nombre="Vilma",
         apellido="Galafassi",
         numero_de_telefono= 47986514,
-        fecha_de_nacimiento="1945-08-17"
+        fecha_de_nacimiento= "1945-08-17"                 #nacimiento_familiar3.strftime("%Y - %m - %d")
     )
 
     familiar1.save() #Guardando los familiares en la base de datos
